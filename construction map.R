2d7894e2422dfcb807264a1eb2f0be225290e1b6
laurence-ren <113177321+laurence-ren@users.noreply.github.com>
2022-11-20 17:58:01 +0100

    script of construction map cleaned

--- construction map.R
+++ construction map.R
@@ -1,13 +1,179 @@
 
-#--- load RColorBrewer ---#
+#### install library 
+
 library(RColorBrewer)
-display.brewer.all()
+library("sf")
+library("rnaturalearth")
+library("rnaturalearthdata")
+require(ggplot2)
+library("ggnewscale")  
+library(ggrepel)
 
-map2 <- get_googlemap()
 
-#=====
-# PLOT
-#=====
+########################################################
+# get right dataset for world map construction 
+########################################################
+
+#===================================
+## read files
+#===================================
+
+requin <- read.csv("attacks.csv") 
+
+req <- requin %>% 
+  select("Year", "Type", "Country", "Area","Activity", "Fatal..Y.N.") %>%
+  filter(Year >= 1930) 
+
+# select columns necessary for the construction of the world map 
+req_map <- req %>%
+  group_by(Country) %>%
+  summarize(nb_attaque = n())
+
+# read base map 
+world <- ne_countries(scale = "medium", returnclass = "sf") 
+world <- world %>% select(admin, geometry) # for countries 
+
+fond <- st_read(dsn = "world-administrative-boundaries/world-administrative-boundaries.shp",
+                stringsAsFactors = F) # for regions 
+
+#===================================
+## modify element 
+#===================================
+
+# change countries names and site to lower 
+req_map$Country <- tolower(req_map$Country)
+world$admin <- tolower(world$admin)
+fond$name <- tolower(fond$name)
+
+# standardise site name 
+req_map[req_map$Country == "columbia"   ,]$Country <- "colombia"
+req_map[req_map$Country == "curacao"    ,]$Country <- "curaçao"
+req_map[req_map$Country == "egypt / israel"    ,]$Country <- "egypt" # israel 
+req_map[req_map$Country == "grand cayman",]$Country <- "cayman islands"
+req_map[req_map$Country == "iran / iraq",]$Country <- "iran" # "iraq"
+req_map[req_map$Country == "italy / croatia" ,]$Country <- "iran" # "italy"
+req_map[req_map$Country == "maldive islands" ,]$Country <- "maldives" 
+req_map[req_map$Country == "micronesia" ,]$Country <- "federated states of micronesia" 
+req_map[req_map$Country == "nicaragua "  ,]$Country <- "nicaragua"  
+req_map[req_map$Country == "palestinian territories"  ,]$Country <- "palestine"  
+req_map[req_map$Country == "solomon islands / vanuatu"  ,]$Country <- "solomon islands"  # et vanatu 
+req_map[req_map$Country == "tanzania"  ,]$Country <- "united republic of tanzania"
+req_map[req_map$Country == "sudan?"  ,]$Country <- "sudan"
+req_map[req_map$Country == "trinidad & tobago"  ,]$Country <- "trinidad and tobago"
+req_map[req_map$Country == "turks & caicos"  ,]$Country <- "turks and caicos island"
+req_map[req_map$Country == "united arab emirates (uae)"   ,]$Country <- "united arab emirates"
+req_map[req_map$Country == "usa"   ,]$Country <- "united states of america"
+req_map[req_map$Country ==  "western samoa"   ,]$Country <- "samoa"
+
+
+# get site present in csv "world"
+a <- req_map$Country
+b <- world$admin
+present_in_world <- a[which(a %in% b  == T)] 
+
+# get site present in csv "fond"
+a <- as.vector(factor(req_map$Country))
+bbis <- as.vector(factor(fond$name))
+present_in_fond <- a[which(a %in% bbis  == T)] 
+
+# site present in both 
+present <- union(present_in_world , present_in_fond )
+dif <- setdiff(present_in_world ,present_in_fond) # site present in fond and not in world 
+
+#===================================
+## get the right dataset for countries and regiop
+#===================================
+
+# find geometry necessary for some site in the base fond 
+geometry_fond <- fond %>%
+  filter(fond$name %in% dif) %>%
+  select("admin"= name, "geometry")
+
+geom_init <- rbind(world, geometry_fond)
+
+# for every site, calculate totale attacks 
+attack_by_site <- req_map %>%
+  group_by(Country) %>%
+  summarise(nb_att_tot = sum(nb_attaque)) %>%
+  select("admin"= Country, "attacks"= nb_att_tot)
+
+# get dataset with geometry and total attacks of every site
+geom <- merge(geom_init , attack_by_site, by="admin", all.x=T)
+
+
+########################################################
+## make dataset for oceans and seas 
+########################################################
+
+# standardisation 
+req_map[req_map$Country ==  "north atlantic ocean " ,]$Country <-"north atlantic ocean"
+req_map[req_map$Country ==  "pacific ocean " ,]$Country <-"pacific ocean"
+req_map[req_map$Country ==  "red sea / indian ocean" ,]$Country <-"indian ocean"
+req_map[req_map$Country ==  "central pacific" ,]$Country <-"pacific ocean"
+req_map[req_map$Country ==  "southwest pacific ocean" ,]$Country <-"south pacific ocean"
+req_map[req_map$Country ==  "pacific ocean" ,]$Country <-"mid-pacific ocean"
+
+
+# locate ocean/sea/gulf site present in attacks.csv
+oce_mer <- c("caribbean sea" , "gulf of aden", "indian ocean",
+             "mid atlantic ocean" ,"mid-pacifc ocean",
+             "north atlantic ocean","north pacific ocean","north sea", 
+             "northern arabian sea", "mid-pacific ocean",
+             "persian gulf", "red sea", "south atlantic ocean", "south china sea",
+             "south pacific ocean" )
+
+#=========================================================================================
+## get element necessary for every site: latitude, longitude, nb tot of attacks 
+#=========================================================================================
+
+lat <- c(14.939588401460744, 12.579150350346652, -31.65256869194269, 
+         0.0012874607478224015, 0.5168641856999522,
+         37.55840704949698,26.740062627514437, 56.89226369685983, 
+         13.52913765091261, 6.647930428748582,
+         27.39827299362649, 21.182319274300287, -32.004663545742, 17.010418251252442, 
+         -34.74671641096358)
+
+
+long <- c(-74.7792643588162, 47.96353180808757,81.67236879838734, 
+          -19.9999143509654, -150.4849986612673,
+          -39.08714830907774, -169.80601017384916, 3.676283675031177,
+          64.31588270602367, -163.13809314592606,
+          51.41987343745529, 38.424038461264196, -15.840314716043286, 113.41267801349872,
+          -139.57163512902778)
+
+
+
+attacks_oce_mer <- req_map %>%
+  filter(Country %in% oce_mer) %>%
+  group_by(Country) %>% 
+  summarise(nb_att_tot = sum(nb_attaque)) %>%
+  select("oce_mer"= Country, "attacks"= nb_att_tot)
+
+#========================================
+# make dataframe
+#========================================
+
+omg_init <- data.frame(oce_mer, lat, long)
+omg <- merge(omg_init, attacks_oce_mer, by="oce_mer")
+
+geom_test <- data.frame("admin"= c(geom$admin, omg$oce_mer),
+                        "lat" = c(rep(NA, 241), omg$lat), 
+                        "long" = c(rep(NA, 241), omg$long),
+                        "attacks" = c(geom$attacks, omg$attacks))
+
+
+########################################################
+## still have some site without geometry or localisation
+########################################################
+
+all.countries <- req_map$Country
+all.countries[which( ( all.countries  %in% oce_mer| all.countries%in% present  )== F)]
+
+
+########################################################
+## world map
+########################################################
+
 map <- geom %>% 
   ggplot() + 
   geom_sf(aes(fill = attacks)) +
@@ -17,13 +183,16 @@ map <- geom %>%
                        breaks = c(1, 7, 55, 403, 1954),
                        na.value="#edeef2") 
   # by default, scale_fill_distiller direction is 1
-  
-# ======
+
+map
+
+# ==================
 # Formatted map
-# =====
+# =================
+
 map2 <- map +
   theme_void() +
-  ggtitle("Nombre d'attaque par les requins entre 1930 et 2014") +
+  ggtitle("Nombre d'attaque par les requins entre 1930 et 2018") +
   #guides(fill = guide_legend(reverse = T))+
   theme(text = element_text(family = 'Gill Sans', color = '#EEEEEE')   # mettre le texte en blanc et type de texte
         ,plot.title = element_text(size = 26.5)
@@ -31,18 +200,13 @@ map2 <- map +
         ,plot.background = element_rect(fill = '#333333') # ajouter couleur de fond 
         ,legend.position="bottom")
 
-# add xlab("Longitude") + ylab("Latitude") 
 map2
 
 
 
-####### ============
-####### add oce_mer
-####### ===========
-
-install.packages("ggnewscale")         
-library("ggnewscale")  
-library(ggrepel)
+# ============================
+# add oceans /sea/ gulf point 
+# ===========================
 
 map3 <- map2 +
   new_scale_color() +
@@ -59,172 +223,5 @@ map3 <- map2 +
   
 map3
 
-############################################################
-##### 2e graphe: fatal ou non selon type d’attaque (camenbert) 
-############################################################
-
-req$Country <- tolower(req$Country)
-
-req[req$Country == "columbia"   ,]$Country <- "colombia"
-req[req$Country == "curacao"    ,]$Country <- "curaçao"
-req[req$Country == "egypt / israel"    ,]$Country <- "egypt" # israel 
-req[req$Country == "grand cayman",]$Country <- "cayman islands"
-req[req$Country == "iran / iraq",]$Country <- "iran" # "iraq"
-req[req$Country == "italy / croatia" ,]$Country <- "iran" # "italy"
-req[req$Country == "maldive islands" ,]$Country <- "maldives" 
-req[req$Country == "micronesia" ,]$Country <- "federated states of micronesia" 
-req[req$Country == "nicaragua "  ,]$Country <- "nicaragua"  
-req[req$Country == "palestinian territories"  ,]$Country <- "palestine"  
-req[req$Country == "solomon islands / vanuatu"  ,]$Country <- "solomon islands"  # et vanatu 
-req[req$Country == "tanzania"  ,]$Country <- "united republic of tanzania"
-req[req$Country == "sudan?"  ,]$Country <- "sudan"
-req[req$Country == "trinidad & tobago"  ,]$Country <- "trinidad and tobago"
-req[req$Country == "turks & caicos"  ,]$Country <- "turks and caicos island"
-req[req$Country == "united arab emirates (uae)"   ,]$Country <- "united arab emirates"
-req[req$Country == "usa"   ,]$Country <- "united states of america"
-req[req$Country ==  "western samoa"   ,]$Country <- "samoa"
-
-req[req$Country ==  "north atlantic ocean " ,]$Country <-"north atlantic ocean"
-req[req$Country ==  "pacific ocean " ,]$Country <-"pacific ocean"
-req[req$Country ==  "red sea / indian ocean" ,]$Country <-"indian ocean"
-req[req$Country ==  "central pacific" ,]$Country <-"pacific ocean"
-req[req$Country ==  "southwest pacific ocean" ,]$Country <-"south pacific ocean"
-req[req$Country ==  "pacific ocean" ,]$Country <-"mid-pacific ocean"
-
-# preparation du jeu de donnée 
-req_pie <- req %>%
-  filter(Fatal..Y.N. == "Y") %>%
-  group_by(Activity) %>%
-  summarise(Effectif = n(), Pourcentage = Effectif/353) 
-
-
-# diving 
-for (i in (1:nrow(req_pie))) {
-  if (grepl("diving", req_pie$Activity[i]) | (grepl("Diving", req_pie$Activity[i])) |
-                                              (grepl("Dived", req_pie$Activity[i]))) {
-    req_pie$Activity[i] <- "Diving"
-  } 
-}
-
-# fishing
-for (i in (1:nrow(req_pie))) {
-  if (grepl("fishing", req_pie$Activity[i]) | (grepl("Fishing", req_pie$Activity[i])) |
-      (grepl("lobsters", req_pie$Activity[i])) |
-      grepl("fish", req_pie$Activity[i]) |
-      grepl("p\xeacheur", req_pie$Activity[i])
-      ) {
-    req_pie$Activity[i] <- "fishing"
-  } 
-}
-
-# air or sea disaster
-for (i in (1:nrow(req_pie))) {
-  if (grepl("disaster", req_pie$Activity[i]) | grepl("Disaster", req_pie$Activity[i])| 
-      (grepl("air", req_pie$Activity[i])) | (grepl("plane", req_pie$Activity[i]))
-      | (grepl("757", req_pie$Activity[i]))|
-      grepl("hurricane", req_pie$Activity[i])
-      ) {
-    req_pie$Activity[i] <- "Air/Sea disaster"
-  } 
-}
-
-# boat/ship accident de bateau 
-for (i in (1:nrow(req_pie))) {
-  if (grepl("Boat", req_pie$Activity[i]) | (grepl("boat", req_pie$Activity[i]))
-      | (grepl("ship", req_pie$Activity[i])) | (grepl("Fell", req_pie$Activity[i]))
-      | (grepl("Vessel", req_pie$Activity[i])) |
-      grepl("sank", req_pie$Activity[i])|
-      grepl("promotional film ", req_pie$Activity[i]) |
-      grepl("Ferry", req_pie$Activity[i])|
-      grepl("ferry", req_pie$Activity[i])|
-      grepl("capsiz", req_pie$Activity[i]) |
-      grepl("MV Dona Marilyn", req_pie$Activity[i]) |
-      grepl("schooner Elizabeth", req_pie$Activity[i]) |
-      grepl("Towing", req_pie$Activity[i]) |
-      grepl("Tzenny Chandris", req_pie$Activity[i]) |
-      grepl("Wreck", req_pie$Activity[i]) |
-      grepl("wreck", req_pie$Activity[i]) |
-      grepl("dinghy", req_pie$Activity[i]) |
-      grepl("yacht", req_pie$Activity[i]) 
-      ) {
-    req_pie$Activity[i] <- "boat/ship accident"
-  } 
-}
-
-# sport 
-for (i in (1:nrow(req_pie))) {
-  if (grepl("surf", req_pie$Activity[i]) | grepl("Surf", req_pie$Activity[i]) |(grepl("boarding", req_pie$Activity[i]))|
-      grepl("Boarding", req_pie$Activity[i]) |
-      (grepl("batching", req_pie$Activity[i]))| (grepl("Bathing", req_pie$Activity[i])) |
-      (grepl("bathing", req_pie$Activity[i])) |
-      (grepl("Swim", req_pie$Activity[i]))| (grepl("swim", req_pie$Activity[i])) |
-      (grepl("fell", req_pie$Activity[i])) | (grepl("Paddling", req_pie$Activity[i]))| 
-      (grepl("Paddling", req_pie$Activity[i])) | (grepl("water", req_pie$Activity[i]))
-      | (grepl("Snorkeling", req_pie$Activity[i]))
-      | (grepl("Sitting", req_pie$Activity[i])) | 
-      grepl("Happy Jack", req_pie$Activity[i]) |
-      grepl("Bending", req_pie$Activity[i]) |
-      grepl("Canoe", req_pie$Activity[i]) |
-      grepl("paddling", req_pie$Activity[i]) |
-      grepl("Kayak", req_pie$Activity[i]) |
-      grepl("Lifesaving", req_pie$Activity[i]) |
-      grepl("Snorkeling", req_pie$Activity[i]) |
-      grepl("Stamding", req_pie$Activity[i])|
-      grepl("Wading", req_pie$Activity[i]) |
-      grepl("Wash", req_pie$Activity[i]) |
-      grepl("Workong", req_pie$Activity[i])| 
-      grepl("McIver", req_pie$Activity[i]) |
-      grepl("Standing", req_pie$Activity[i]) |
-      grepl("Crossing the bay", req_pie$Activity[i])|
-      grepl("Walking", req_pie$Activity[i]) |
-      grepl("kill a shark", req_pie$Activity[i]) |
-      grepl("Removing shark", req_pie$Activity[i]) 
-      ) {
-    req_pie$Activity[i] <- "sport/ activity" }
-}
-
-
-
-# adrift
-for (i in (1:nrow(req_pie))) {
-  if (grepl("Adrift", req_pie$Activity[i]) | grepl("adrift", req_pie$Activity[i]) |
-      grepl("Floating", req_pie$Activity[i])) {
-    req_pie$Activity[i] <- "Adrift"
-  } 
-}
-
-# murder /  war 
-for (i in (1:nrow(req_pie))) {
-  if (grepl("Murder", req_pie$Activity[i]) ||
-      grepl("submarine", req_pie$Activity[i]) |
-      grepl("American", req_pie$Activity[i]) |
-      grepl("torpedoed", req_pie$Activity[i]) |
-      grepl("Torpedoed", req_pie$Activity[i]) |
-      grepl("soldiers", req_pie$Activity[i]) |
-      grepl("Escaping", req_pie$Activity[i]) |
-      grepl("Japanese", req_pie$Activity[i]) |
-      grepl("Japan", req_pie$Activity[i]) |
-      grepl("Sinking", req_pie$Activity[i]) |
-      grepl("SS", req_pie$Activity[i]) |
-      grepl("foundered", req_pie$Activity[i])  |
-      grepl("US", req_pie$Activity[i])
-      ) {
-    req_pie$Activity[i] <- "murder /  war "
-  } 
-}
-
-req_pie_final <- req_pie %>%
-  group_by(Activity) %>%
-  summarise(Effectif = n(), Pourcentage = Effectif/353) 
-
-req_pie_final <- req_pie_final[-1,]
-
-
-ggplot(data = req_pie_final, aes(x="", y=Pourcentage, fill= Activity)) +
-  geom_bar(stat="identity", width=1, color= "white") +
-  coord_polar("y", start=0) +
-  theme_void()
-
-map3
 
 
